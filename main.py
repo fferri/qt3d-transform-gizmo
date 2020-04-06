1c16f4cb2057868707344e771d083306eb4ddb86
Justin Weber <onlyjus@gmail.com>
2020-04-06 09:04:57 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+import os
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQuick import QQuickView
+from PyQt5.QtCore import QUrl
+
+if __name__ == '__main__':
+    app = QGuiApplication(sys.argv)
+    view = QQuickView()
+    view.setResizeMode(QQuickView.SizeRootObjectToView)
+
+    qmlFile = os.path.join(os.path.dirname(__file__), 'main.qml')
+    view.setSource(QUrl.fromLocalFile(os.path.abspath(qmlFile)))
+    if view.status() == QQuickView.Error:
+        sys.exit(-1)
+    view.show()
+
+    app.exec_()
+    # Deleting the view before it goes out of scope is required to make
+    # sure all child QML instances are destroyed in the correct order.
+    del view
